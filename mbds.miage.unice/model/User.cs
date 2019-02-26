76e7858727a135057d6e7bab7347f27efcdd4826
chendatao407786 <datao.chen@etu.unice.fr>
2019-02-26 01:10:33 +0100

    I have a pen

---
+++ mbds.miage.unice/model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbds.miage.unice.model
+{
+    class User
+    {
+        private string _name;
+        private string _id;
+        private string _srcImage;
+        public User(string name, string id, string srcImage)
+        {
+            _name = name;
+            _id = id;
+            _srcImage = srcImage;
+        }
+        public string Name { get => _name; set => _name = value; }
+        public string Id { get => _id; set => _id = value; }
+        public string SrcImage { get => _srcImage; set => _srcImage = value; }
+    }
+}
--- mbds.miage.unice/pages/chat/ChatWindow.xaml.cs
+++ mbds.miage.unice/pages/chat/ChatWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿//using mbds.miage.unice.model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,31 @@ namespace mbds.miage.unice.pages.chat
     /// </summary>
     public partial class ChatWindow : Window
     {
+        ObservableCollection<User> userList = new ObservableCollection<User>();
+        public ObservableCollection<User> UserList { get => userList; set => userList = value; }
+
         public ChatWindow()
         {
+            this.DataContext = this;
+            User user1 = new User("Datao", "chendatao1126", "http://192.168.0.10:8888/tpgrails/images/chen.jpg");
+            userList.Add(user1);
             InitializeComponent();
         }
+
+    }
+    public class User
+    {
+        private string _name;
+        private string _id;
+        private string _srcImage;
+        public User(string name, string id, string srcImage)
+        {
+            _name = name;
+            _id = id;
+            _srcImage = srcImage;
+        }
+        public string Name { get => _name; set => _name = value; }
+        public string Id { get => _id; set => _id = value; }
+        public string SrcImage { get => _srcImage; set => _srcImage = value; }
     }
 }
