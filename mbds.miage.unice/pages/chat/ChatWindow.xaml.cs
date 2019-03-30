76b047c4c1bee6432f0dbe1b7c61743d30a3cf64
Thiaw <thiawma@hotmail.fr>
2019-03-30 18:43:01 +0100

    add thiaw

--- mbds.miage.unice/pages/chat/ChatWindow.xaml.cs
+++ mbds.miage.unice/pages/chat/ChatWindow.xaml.cs
@@ -26,8 +26,11 @@ namespace mbds.miage.unice.pages.chat
             this.DataContext = this;
             User user1 = new User("Datao", "chendatao1126", "http://192.168.0.10:8888/tpgrails/images/chen.jpg");
             User user2 = new User("Serina", "serina.li", "http://192.168.0.10:8888/tpgrails/images/30704260_10215948242607669_7236327905523141774_n.jpg");
+            User user3 = new User("thiaw", "thiaw", "http://192.168.0.10:8888/tpgrails/images/me.png");
+
             userList.Add(user1);
             userList.Add(user2);
+            userList.Add(user3);
             InitializeComponent();
             Loaded += (s, e) => Keyboard.Focus(Sent);
         }
